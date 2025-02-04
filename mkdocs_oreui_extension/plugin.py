62bd09d07fa5d7d3172c2f4a8d3b50d70956b9f6
Cdm2883 <Cdm2883@hotmail.com>
2025-02-04 16:34:33 +0800

    feat: add file hash

--- mkdocs_oreui_extension/plugin.py
+++ mkdocs_oreui_extension/plugin.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 from mkdocs.config import config_options
 from mkdocs.config.base import Config as BaseConfig
 from mkdocs.plugins import BasePlugin
@@ -6,7 +7,7 @@ from mkdocs.commands.serve import LiveReloadServer
 from mkdocs.utils import write_file
 import sass
 
-OUTPUT_CSS_PATHS = ["assets", "stylesheets", "oreui.min.css"]
+OUTPUT_CSS_PATHS = ["assets", "stylesheets", "oreui"]
 
 
 class ExtensionConfig(BaseConfig):
@@ -16,6 +17,9 @@ class ExtensionConfig(BaseConfig):
 class OreUiExtension(BasePlugin[ExtensionConfig]):
     resolve_assets_root: str
     resolve_site_root: str
+    css_compiled_paths = OUTPUT_CSS_PATHS[:-1] + [OUTPUT_CSS_PATHS[-1] + '.min.css']
+    css_compiled: str
+    is_serve_mode = False
 
     def on_config(self, config: BaseConfig, **kwargs) -> BaseConfig:
         self.resolve_assets_root = (
@@ -24,24 +28,33 @@ class OreUiExtension(BasePlugin[ExtensionConfig]):
             else os.path.dirname(os.path.abspath(__file__))
         )
         self.resolve_site_root = config['site_dir']
-        config["extra_css"] = ['/'.join(OUTPUT_CSS_PATHS)] + config['extra_css']
+        self.compile_scss()
+        config['extra_css'] = ['/'.join(self.css_compiled_paths)] + config['extra_css']
         return config
 
     def on_post_build(self, config, **kwargs) -> None:
-        css_path = self.resolve_site(*OUTPUT_CSS_PATHS)
-        if not os.path.exists(css_path): self.compile_scss()
+        self.output_css()
 
     def on_serve(self, server: LiveReloadServer, config, **kwargs):
+        self.is_serve_mode = True
         if not self.config.internal_dev_watchdog: return
         scss_paths = self.resolve_assets('stylesheets')
-        server.watch(scss_paths, self.compile_scss)
+        server.watch(scss_paths, self.internal_dev_watchdog)
+
+    def internal_dev_watchdog(self):
+        self.compile_scss()
+        self.output_css()
 
     def compile_scss(self):
         sass_input = self.resolve_assets('stylesheets', 'index.scss')
-        css_output = self.resolve_site(*OUTPUT_CSS_PATHS)
+        self.css_compiled = sass.compile(filename=sass_input, output_style='compressed').encode('utf-8')
+        if self.config.internal_dev_watchdog and self.is_serve_mode: return
+        file_hash = hashlib.md5(self.css_compiled).hexdigest()[:8]
+        self.css_compiled_paths = OUTPUT_CSS_PATHS[:-1] + [OUTPUT_CSS_PATHS[-1] + '.' + file_hash + '.min.css']
 
-        compiled_css = sass.compile(filename=sass_input, output_style='compressed')
-        write_file(compiled_css.encode('utf-8'), css_output)
+    def output_css(self):
+        css_output = self.resolve_site(*self.css_compiled_paths)
+        write_file(self.css_compiled, css_output)
 
     def resolve_assets(self, *paths: str):
         return os.path.join(self.resolve_assets_root, *paths)
